ab633ffa95a048789cca483c508d33a2ef4b5c5a
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-23 15:52:54 +0530

    Custom hook convention

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
-function customLogger(counter){
+function useCustomLogger(counter){// to change this function to custom hook we need to use "use" convention,at the end of day it is nothing but a function
   console.log("from custom logger",counter)
 }
 function App() {
   const [counter, setCounter] = useState(0)
 
-customLogger(counter)
+useCustomLogger(counter)
   return (
     <>
      <h1>Custom Hooks {counter}</h1>
