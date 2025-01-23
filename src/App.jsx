c13b8c7511bbf45006e7baf8a02c88f593ab8a1f
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-23 16:12:02 +0530

    Made a custom hook useCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 
 import './App.css'
-function useCustomLogger(counter){// to change this function to custom hook we need to use "use" convention,at the end of day it is nothing but a function
+function useLogger(counter){// to change this function to custom hook we need to use "use" convention,at the end of day it is nothing but a function
   console.log("from custom logger",counter)
 }
 function App() {
+  
+function useCounter(){
   const [counter, setCounter] = useState(0)
-
-useCustomLogger(counter)
+  const incrementCounter=()=>setCounter(count=>count+1)
+  const decrementCounter=()=>setCounter(count=>count-1)
+return {counter,incrementCounter,decrementCounter}
+}
+const {counter,incrementCounter,decrementCounter}=useCounter()
+useLogger(counter)
   return (
     <>
      <h1>Custom Hooks {counter}</h1>
-     <button onClick={()=>setCounter(count=>count+1)}>+</button>
-     <button onClick={()=>setCounter(count=>count-1)}>-</button>
+     <button onClick={incrementCounter}>+</button>
+     <button onClick={decrementCounter}>-</button>
     </>
   )
 }
