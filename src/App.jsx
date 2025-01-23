a4aa0b3dab4f5715a1c4872a170c307af9e523e6
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-23 15:50:06 +0530

    Counter set up using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+function customLogger(counter){
+  console.log("from custom logger",counter)
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
 
+customLogger(counter)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Custom Hooks {counter}</h1>
+     <button onClick={()=>setCounter(count=>count+1)}>+</button>
+     <button onClick={()=>setCounter(count=>count-1)}>-</button>
     </>
   )
 }
